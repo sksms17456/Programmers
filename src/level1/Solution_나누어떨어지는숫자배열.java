87bf1986f34bcad831b5b2297b38c1fe88595513
박현빈 <sksms17456@naver.com>
2019-06-26 17:08:55 +0900

    190626

--- src/level1/Solution_나누어떨어지는숫자배열.java
+++ src/level1/Solution_나누어떨어지는숫자배열.java
@@ -28,23 +28,23 @@ import java.util.Collections;
 
 public class Solution_나누어떨어지는숫자배열 {
 	public int[] solution(int[] arr, int divisor) {
-		ArrayList<Integer> list = new ArrayList<>();
+		ArrayList<Integer> numlist = new ArrayList<>();
 		
 		for(int i=0; i<arr.length; i++) {
 			if(arr[i]%divisor==0) {
-				list.add(arr[i]);
+				numlist.add(arr[i]);
 			}
 		}
-		int[] answer = new int[list.size()];
-		if(list.size()==0) {
+		int[] answer = new int[numlist.size()];
+		if(numlist.size()==0) {
 			answer = new int[1];
 			answer[0] = -1;
 			return answer;
 		}
-		Collections.sort(list);
-		answer = new int[list.size()];
+		Collections.sort(numlist);
+		answer = new int[numlist.size()];
 		for(int i=0; i<answer.length; i++) {
-			answer[i] = list.get(i);
+			answer[i] = numlist.get(i);
 		}
 		return answer;
 	}
