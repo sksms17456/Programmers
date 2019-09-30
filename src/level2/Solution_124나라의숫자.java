80eca6e6f148bbc90d4ebc70b6789dc309966d1a
박현빈 <sksms17456@naver.com>
2019-09-30 18:00:59 +0900

    [MOD] 190930_level2

---
+++ src/level2/Solution_124나라의숫자.java
@@ -0,0 +1,55 @@
+//문제 설명
+//124 나라가 있습니다. 124 나라에서는 10진법이 아닌 다음과 같은 자신들만의 규칙으로 수를 표현합니다.
+//
+//124 나라에는 자연수만 존재합니다.
+//124 나라에는 모든 수를 표현할 때 1, 2, 4만 사용합니다.
+//예를 들어서 124 나라에서 사용하는 숫자는 다음과 같이 변환됩니다.
+//
+//10진법	124 나라	10진법	124 나라
+//1	1	6	14
+//2	2	7	21
+//3	4	8	22
+//4	11	9	24
+//5	12	10	41
+//자연수 n이 매개변수로 주어질 때, n을 124 나라에서 사용하는 숫자로 바꾼 값을 return 하도록 solution 함수를 완성해 주세요.
+//
+//제한사항
+//n은 500,000,000이하의 자연수 입니다.
+package level2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution_124나라의숫자 {
+	public static void main(String[] args) {
+//		int n = 1;
+//		int n = 2;
+//		int n = 3;
+//		int n = 4;
+		for(int n=1; n<50; n++) {
+			System.out.println("n : "+n + " : "  + solution(n));
+			
+		}
+//			System.out.println(solution(10));
+	}
+
+	static String solution(int n) {
+		Map<Integer, String> map = new HashMap<>();
+		map.put(0, "4");
+		map.put(1, "1");
+		map.put(2, "2");
+		int mod = n%3;
+		if(mod==0) {
+			n-=3;
+		}else {
+			n -= mod;			
+		}
+		int v = 9;
+		StringBuilder sb = new StringBuilder();
+		sb.append(map.get(mod));
+		while(true) {
+		}
+		String answer = sb.toString();
+		return answer;
+	}
+}
